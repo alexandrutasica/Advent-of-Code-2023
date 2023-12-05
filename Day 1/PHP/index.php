46b22af81610b5663068bcb32f479a21bc129785
Alexandru Tasica <ext.tech.alex@lemon.markets>
2023-12-05 17:28:43 +0200

    php day 1-2

--- Day 1/PHP/index.php
+++ Day 1/PHP/index.php
@@ -3,7 +3,12 @@
 $input = file_get_contents(__DIR__ . "/../input.txt");
 $lines = explode("\n", $input);
 
+$needle = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+$replace = ["one1one", "two2two", "three3three", "four4four", "five5five", "six6six", "seven7seven", "eight8eight", "nine9nine"];
+
 function processLine(int $acc, string $line) {
+  global $needle, $replace;
+  $line = str_replace($needle, $replace, $line);
   preg_match_all('/\d/', $line, $matches);
   $first = $matches[0][0];
   $last = $matches[0][ count($matches[0]) - 1 ];

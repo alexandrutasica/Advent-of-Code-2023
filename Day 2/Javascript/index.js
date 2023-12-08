f22b9f247aee47e6898378eb3356ef8b2d73cb11
Alexandru Tasica <ext.tech.alex@lemon.markets>
2023-12-08 18:16:21 +0200

    js day 2-1

---
+++ Day 2/Javascript/index.js
@@ -0,0 +1,52 @@
+var fs = require("fs"),
+  path = require("path"),
+  filePath = path.join(__dirname, "../input.txt");
+
+const input = fs.readFileSync(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  input = data;
+});
+const linesArray = input.split("\n");
+
+const maxCubesPerColor = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function mapValidGames(lines) {
+  const validGames = [];
+  lines.forEach((element) => {
+    const lineSplitted = element.split(":");
+    const gameNumber = lineSplitted[0].replace("Game ", "");
+    const rounds = lineSplitted[1].trim().split("; ");
+    let gameValid = true;
+    for (let i = 0; i < rounds.length && gameValid; i++) {
+      const roundMoves = rounds[i].split(", ");
+      for (
+        let moveIndex = 0;
+        moveIndex < roundMoves.length && gameValid;
+        moveIndex++
+      ) {
+        const move = roundMoves[moveIndex].split(" ");
+        if (maxCubesPerColor[move[1]] < move[0]) {
+          gameValid = false;
+        }
+      }
+    }
+    if (gameValid) {
+      validGames.push(gameNumber);
+    }
+  });
+  return validGames;
+}
+
+console.log(
+  "Sum of valid games: ",
+  mapValidGames(linesArray).reduce((acc, item) => acc + parseInt(item, 10), 0)
+);
+
+console.log(mapValidGames(linesArray));

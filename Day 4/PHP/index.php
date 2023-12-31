43e2ab8489503f92da887cc3cc075459d46f573b
Alexandru Tasica <ext.tech.alex@lemon.markets>
2023-12-31 18:13:51 +0100

    php day 4-2

--- Day 4/PHP/index.php
+++ Day 4/PHP/index.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 /*
 The Elf leads you over to the pile of colorful cards. There, you discover dozens of scratchcards, all with their opaque covering already scratched off. Picking one up, it looks like each card has two lists of numbers separated by a vertical bar (|): a list of winning numbers and then a list of numbers you have. You organize the information into a table (your puzzle input).
 
@@ -27,4 +27,32 @@ function solution($data) {
   return $total;
 }
 
+$originalData = $lines;
+$originalDataCount = count($lines);
+function solutionPart2($data) {
+  global $originalData, $originalDataCount;
+
+  $total = count($data);
+  for($lineIndex = 0; $lineIndex < count($data); $lineIndex++) {
+    $splittedData = explode(" | ", $data[$lineIndex]);
+    // Without array_values, it won't reset the key index number of each values so when filtering you might have indexes: 0, 3, 4, 6
+    $myNumbers = array_values(array_filter( explode( " ", $splittedData[1] ), fn($item) => strlen(trim($item)) > 0 ));
+    $winningNumbers = array_filter( explode( " ", explode( ":", $splittedData[0] )[1] ), fn($item) => strlen(trim($item)) > 0 );
+    $currentGame = (int) str_replace("Card ", "", explode(":", $splittedData[0])[0] );
+    $wonCards = [];
+    $currentWonIndex = $currentGame;
+    for($numberIndex = 0; $numberIndex < count($myNumbers) && $currentWonIndex < $originalDataCount; $numberIndex++) {
+      if(in_array($myNumbers[$numberIndex], $winningNumbers)) {
+        $wonCards[] = $originalData[$currentWonIndex];
+        $currentWonIndex += 1;
+      }
+    }
+    if (count($wonCards) > 0) {
+      $total += solutionPart2($wonCards);
+    }
+  }
+  return $total;
+}
+
 echo 'Part 1: ' . solution($lines) . "\n";
+echo 'Part 2: ' . solutionPart2($lines) . "\n";
